846d7a0ca57edd9dbbcdc6f86e20d8062de29a45
Simon <seolivares@gmail.com>
2024-09-26 16:04:43 -0300

    Se genera el Ejercicio2

--- src/ejercicios/Ejercicio1.java
+++ src/ejercicios/Ejercicio1.java
@@ -2,6 +2,14 @@ package ejercicios;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO......!!!!!");
+        /*
+            1. Suma de dos números
+•	        Descripción: Dados los números a = 10 y b = 20, calcula la suma de ambos y muestra el resultado.
+•	        Variables: a = 10, b = 20
+         */
+
+        int a = 10, b = 20;
+
+        System.out.println("La suma de a = " + a + " + b = " + b + " es: " + (a+b));
     }
 }
---
+++ src/ejercicios/Ejercicio2.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        /*
+          2. Producto de dos números
+•	      Descripción: Dados los números a = 7 y b = 8, calcula el producto de ambos y muestra el resultado.
+•	      Variables: a = 7, b = 8
+        */
+
+        int a = 7, b = 8;
+
+        System.out.println("El producto de a = " + a + " * b = " + b + " es: " + (a*b));
+    }
+}
