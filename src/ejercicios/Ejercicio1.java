6ffda97fa0ecc549f5ac8f248f0ca3e50f35f77b
Simon <seolivares@gmail.com>
2024-09-26 16:39:44 -0300

    Se actualizan los ejercicios

--- src/ejercicios/Ejercicio1.java
+++ src/ejercicios/Ejercicio1.java
@@ -1,5 +1,4 @@
 package ejercicios;
-
 public class Ejercicio1 {
     public static void main(String[] args) {
         /*
--- src/ejercicios/Ejercicio2.java
+++ src/ejercicios/Ejercicio2.java
@@ -3,9 +3,9 @@ package ejercicios;
 public class Ejercicio2 {
     public static void main(String[] args) {
         /*
-          2. Producto de dos números
-•	      Descripción: Dados los números a = 7 y b = 8, calcula el producto de ambos y muestra el resultado.
-•	      Variables: a = 7, b = 8
+           2. Producto de dos números
+•	       Descripción: Dados los números a = 7 y b = 8, calcula el producto de ambos y muestra el resultado.
+•	       Variables: a = 7, b = 8
         */
 
         int a = 7, b = 8;
